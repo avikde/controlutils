bf8950e0bbd014c1b59b05316e0e4744c8b10c00
Avik De <avikde@gmail.com>
2019-03-18 12:30:53 -0400

    add twistInt

--- py/ControlUtils.py
+++ py/ControlUtils.py
@@ -18,9 +18,25 @@ def rot2lin(phiz):
 	return np.eye(2) + phiz * Skew2
 
 def screw(twistdt):
+	if len(twistdt) != 3:
+		raise 'Only SE(2) implemented'
 	# Assuming SE(2)
 	twistHat = np.vstack((
 		np.hstack((Skew2 * twistdt[2], np.array([[twistdt[0]],[twistdt[1]]]))),
 		np.zeros(3)
 	))
 	return scipy.linalg.expm(twistHat)
+
+def twistInt(q0, twistDes, N, dt):
+	# Integrates up the twist. Should work regardless of SE(2) or SE(3)
+	# This linear map is the "transition" matrix
+	Screw = screw(twistDes * dt)
+	# Apply recursively
+	qtraj = np.zeros((N, len(q0)))
+	qtraj[0, :] = q0
+	for k in range(1, N):
+		# Homegeneous coords
+		qtraj[k, :] = Screw @ np.hstack((qtraj[k-1, 0:2], 1))
+		# Last element is just yaw
+		qtraj[k, 2] = qtraj[k-1, 2] + twistDes[2] * dt
+	return qtraj

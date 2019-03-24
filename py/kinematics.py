804b97d2f7f4b8786dcdb3a4a08b4b96cd331d65
Avik De <avikde@gmail.com>
2019-03-24 09:26:58 -0400

    affine version also works

--- py/kinematics.py
+++ py/kinematics.py
@@ -16,22 +16,8 @@ def rot2lin(phiz):
 	'''Linearization of rotation (small angle)'''
 	return np.eye(2) + phiz * Skew2
 
-def applyTwist(q0, twistDes, dt):
-	'''Integrates up the twist. Should work regardless of SE(2) or SE(3).
-	'''
-	if len(twistDes) != 3:
-		raise('Only SE(2) implemented')
-	
-	qnew = np.hstack((
-		# Rotate to align twist to body frame
-		q0[0:2] + (rot2(q0[2]) @ twistDes[0:2]) * dt,
-		# Last element is just yaw
-		q0[2] + twistDes[2] * dt
-	))
-	return qnew
-
-def linearizedKinematics(q, dt):
-	'''Linearized kinematics of applying an SE(n) twist to a pose. (Linearized version of applyTwist)
+def affineKinematics(q, dt):
+	'''Linearized kinematics of applying an SE(n) twist to a pose.
 	q = input pose
 	dt = time to apply twist (should be small for accuracy)
 	Returns B s.t.
@@ -39,10 +25,15 @@ def linearizedKinematics(q, dt):
 	'''
 	if len(q) != 3:
 		raise('Only SE(2) implemented')
-	
-	posColVec = np.array([[q[0]],[q[1]]])
-	# Linearize a homogeneous transformation exp(t skew(V)) qhomog ~= B(q) V for the first two rows
+	# qnext = {q0, q1} + RotationMatrix[q2].{v0, v1} t
 	return dt * np.vstack((
-		np.hstack((np.eye(2), Skew2 @ posColVec)),
+		np.hstack((rot2(q[2]), np.zeros((2,1)))),
 		np.array([0, 0, 1])
 	))
+
+def applyTwist(q0, twistDes, dt):
+	'''Integrates up the twist. Should work regardless of SE(2) or SE(3).
+	'''
+	if len(twistDes) != 3:
+		raise('Only SE(2) implemented')
+	return q0 + affineKinematics(q0, dt) @ twistDes

d3857fa391ec4b7002b402c46b9b0cb6f6c8ba07
Avik De <avikde@gmail.com>
2019-03-25 10:41:02 -0400

    more general rotation

--- py/kinematics.py
+++ py/kinematics.py
@@ -31,6 +31,13 @@ def affineKinematics(q, dt):
 		np.array([0, 0, 1])
 	))
 
+def rot(phi):
+	'''Either planar, or rotation vector for 3D'''
+	if len(phi) == 1:
+		return rot2(phi[0])
+	else:
+		raise NotImplementedError
+
 def applyTwist(q0, twistDes, dt):
 	'''Integrates up the twist. Should work regardless of SE(2) or SE(3).
 	'''

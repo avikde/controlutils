b5d5810ad02508cb23623f7ca3216c804cabf3c0
Avik De <avikde@gmail.com>
2019-03-25 21:19:16 -0400

    update affineKinematics for 3D

--- py/kinematics.py
+++ py/kinematics.py
@@ -28,6 +28,13 @@ def rot2lin(phiz):
 	'''Linearization of rotation (small angle)'''
 	return np.eye(2) + phiz * Skew2
 
+def rot(phi):
+	'''Either planar, or rotation vector for 3D'''
+	if len(phi) == 1:
+		return rot2(phi[0])
+	else:
+		return Rotation.from_rotvec(phi).as_dcm()
+
 def affineKinematics(q, dt):
 	'''Linearized kinematics of applying an SE(n) twist to a pose.
 	q = input pose
@@ -35,20 +42,19 @@ def affineKinematics(q, dt):
 	Returns B s.t.
 	qnext = q + B(q) v
 	'''
-	if len(q) != 3:
-		raise('Only SE(2) implemented')
-	# qnext = {q0, q1} + RotationMatrix[q2].{v0, v1} t
-	return dt * np.vstack((
-		np.hstack((rot2(q[2]), np.zeros((2,1)))),
-		np.array([0, 0, 1])
-	))
-
-def rot(phi):
-	'''Either planar, or rotation vector for 3D'''
-	if len(phi) == 1:
-		return rot2(phi[0])
+	if len(q) == 3:
+		# qnext = {q0, q1} + RotationMatrix[q2].{v0, v1} t
+		return dt * np.vstack((
+			np.hstack((rot2(q[2]), np.zeros((2,1)))),
+			np.array([0, 0, 1])
+		))
+	elif len(q) == 6:
+		return dt * np.vstack((
+			np.hstack((rot(q[3:6]), np.zeros((3,3)))),
+			np.hstack((np.zeros((3,3)), np.eye(3)))
+		))
 	else:
-		return Rotation.from_rotvec(phi).as_dcm()
+		raise 'Size can only be 3 or 6'
 
 def applyTwist(q0, twistDes, dt):
 	'''Integrates up the twist. Should work regardless of SE(2) or SE(3).

eaffe3673651c3e234fc5fc7d4ff4914659f14e2
Avik De <avikde@gmail.com>
2019-03-27 17:59:48 -0400

    add options for 3DOF twist on 6DOF pose

--- py/kinematics.py
+++ py/kinematics.py
@@ -35,7 +35,7 @@ def rot(phi):
 	else:
 		return Rotation.from_rotvec(phi).as_dcm()
 
-def affineKinematics(q, dt):
+def affineKinematics(q, dt, fullTwist=True):
 	'''Linearized kinematics of applying an SE(n) twist to a pose.
 	q = input pose
 	dt = time to apply twist (should be small for accuracy)
@@ -49,10 +49,17 @@ def affineKinematics(q, dt):
 			np.array([0, 0, 1])
 		))
 	elif len(q) == 6:
-		return dt * np.vstack((
-			np.hstack((rot(q[3:6]), np.zeros((3,3)))),
-			np.hstack((np.zeros((3,3)), np.eye(3)))
-		))
+		if fullTwist:
+			return dt * np.vstack((
+				np.hstack((rot(q[3:6]), np.zeros((3,3)))),
+				np.hstack((np.zeros((3,3)), np.eye(3)))
+			))
+		else:
+			# 3DOF twist corresponding to the horizontal plane
+			return dt * np.block([
+				[rot(q[3:6])[:,0:2], np.zeros((3, 1))],
+				[np.zeros((3, 2)), np.array([[0], [0], [1]])]
+			])
 	else:
 		raise 'Size can only be 3 or 6'
 
@@ -61,4 +68,10 @@ def applyTwist(q0, twistDes, dt):
 	'''
 	if len(twistDes) != 3:
 		raise('Only SE(2) implemented')
-	return q0 + affineKinematics(q0, dt) @ twistDes
+	if len(q0) == 3:
+		Btwist = affineKinematics(q0, dt)
+	elif len(q0) == 6:
+		Btwist = affineKinematics(q0, dt, fullTwist=False)
+	else:
+		raise ValueError('Only 3DOF or 6DOF pose supported')
+	return q0 + Btwist @ twistDes

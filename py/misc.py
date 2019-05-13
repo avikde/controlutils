e078871ecbb5fdef67981ea7f8bd422b08f12b9c
Avik De <avikde@gmail.com>
2019-05-13 17:58:26 -0400

    add cuboid drawing function

--- py/misc.py
+++ py/misc.py
@@ -1,4 +1,5 @@
 import autograd.numpy as np
+from scipy.spatial.transform import Rotation
 from . import kinematics as kin
 
 def rectangle(xy, ang, width, height, rawxy=False, **patchkw):
@@ -22,4 +23,36 @@ def rectangle(xy, ang, width, height, rawxy=False, **patchkw):
         corners[i,:] = xy + Ryaw @ di[i,:]
 
     return corners if rawxy else Polygon(corners, **patchkw)
+
+
+def cuboid(center, rotvec, lwh, rawxy=False, **patchkw):
+    '''Vertices for a cuboid
+    '''
+    vertsB = np.array([
+        [-1, -1, -1],
+        [1, -1, -1],
+        [1, 1, -1],
+        [-1, 1, -1],
+        [-1, -1, 1],
+        [1, -1, 1],
+        [1, 1, 1],
+        [-1, 1, 1]]) @ np.diag(lwh)
+    # tested: this adds to each row
+    Z = Rotation.from_rotvec(rotvec).apply(vertsB) + center # this is vertsW
+
+    if rawxy:
+        return Z
+    else:
+        from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+        
+        # list of sides' polygons of figure
+        polysides = [[Z[0], Z[1], Z[2], Z[3]],
+                [Z[4], Z[5], Z[6], Z[7]],
+                [Z[0], Z[1], Z[5], Z[4]],
+                [Z[2], Z[3], Z[7], Z[6]],
+                [Z[1], Z[2], Z[6], Z[5]],
+                [Z[4], Z[7], Z[3], Z[0]]]
+
+        return Z, Poly3DCollection(polysides, **patchkw)
+
 
